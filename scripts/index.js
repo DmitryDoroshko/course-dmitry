5717ef778c1902e10bc4c36cd0acd0019adeed87
Dmitry Doroshko <dmitry.doroshko.99@gmail.com>
2020-01-23 18:27:51 +0200

    Responsive half way

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,1 @@
-let user={};
-
-for(let key in user) {
-    if (user.hasOwnProperty(key)) {
-        document.write("свойства есть");
-    } else {
-        document.write("свойств нет");
-    }
-}
 
